5b513791fb0ab89466bb78f800ce754be4286a0f
Mohammad Arsalan <kingarain7866@example.com>
2026-01-04 23:11:07 +0500

    Day 4: 26_Remove Duplcates from Sorted Array (Easy)

---
+++ Easy/26_Remove Duplicates from Sorted Array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+
+        left = 0
+
+        for right in range(1, len(nums)):
+            if nums[left] != nums[right]:
+                left += 1
+                nums[left] = nums[right]
+        return left+1
+
+
+obj = Solution()
+print(obj.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))  # 5
+print(obj.removeDuplicates([1,1,2,3,4,5,5]))               # 2
