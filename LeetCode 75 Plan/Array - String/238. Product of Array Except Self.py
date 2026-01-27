587d7f5d73d94e38c09d00fab21daa3d2a9ee52f
insightifyofficial1 <Insightify.official1@gmail.com>
2026-01-27 15:47:36 +0500

    Day 21: Product of Array Except Self

--- LeetCode 75 Plan/Array - String/238. Product of Array Except Self.py
+++ LeetCode 75 Plan/Array - String/238. Product of Array Except Self.py
@@ -1,16 +1,25 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        lst = []
-        for i in range(len(nums)):
-            result = 1
-            for j in range(len(nums)):
-                if nums[i]==nums[j]:
-                    continue
-                result *= nums[j]
-            lst.append(result)
-        print(lst)
-
+        n = len(nums)
+        answer = [1] * n
+        
+        # Step 1: Calculate Left Products (Prefix)
+        # We start with 1 because there is nothing to the left of the first element.
+        left_product = 1
+        for i in range(n):
+            answer[i] = left_product
+            left_product *= nums[i] # Update running product for the next person
+            
+        # Current state of 'answer' for input [1, 2, 3, 4]:
+        # [1, 1, 2, 6] -> These are the products of everything to the left
+        
+        # Step 2: Calculate Right Products (Suffix) and multiply
+        right_product = 1
+        for i in range(n - 1, -1, -1): # Loop backwards
+            answer[i] *= right_product # Multiply valid left product by right product
+            right_product *= nums[i]   # Update running product for the next person
+            
+        return answer
 
 obj = Solution()
-obj.productExceptSelf(nums = [-1,1,0,-3,3])
-# i will tomorrow
+print(obj.productExceptSelf( nums = [1,2,3,4]))
