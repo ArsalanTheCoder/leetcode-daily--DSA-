32762352d43f2e7e027cd3b31afa54137e82a21a
ArsalanTheCoder <kingarain7866@gmail.com>
2026-03-01 08:24:49 +0500

    Day 42: Partitioning into Medium Number of Deci-Binary Numbers (Medium)

---
+++ Medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPartitions(self, n):
+        return int(max(n))
+
+
+obj = Solution()
+print(obj.minPartitions( n = "27346209830709182346"))
+
+# class Solution:
+#     def minPartitions(self, n: str) -> int:
+#         for i in range(9, 0, -1):
+#             if str(i) in n:
+#                 return i
+#         return 0
